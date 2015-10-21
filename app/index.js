98344c052ad53ff10bdab3b7303c246f66fbee76
Jonas Myrlund <myrlund@gmail.com>
2015-10-21 13:29:20 +0200

    Build the shell of a one day historical React application.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,25 @@
-[1, 2, 3].forEach(n => console.log(`ES6'd ${n} times`));
+import React from "react";
+import ReactDOM from "react-dom";
+import StyleSheet from "stilr";
+
+import recordVoice from "../lib/recordVoice";
+
+const styles = StyleSheet.create({
+  header: {
+    color: "red",
+    fontSize: 50,
+    textAlign: "center",
+  },
+});
+
+class App extends React.Component {
+  render() {
+    return <h1 className={styles.header}>Web Music Maker 2K</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
+
+let stylesheet = document.createElement('style');
+stylesheet.textContent = StyleSheet.render();
+document.head.appendChild(stylesheet);
